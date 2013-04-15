19cb5db9853760eaf99a14ff2cf986bca9edc572
Todd Thomas <todd.thomas@openlogic.com>
2013-04-15 01:02:06 -0600

    Fix problem where logger couldn't be set on SignedXml.

--- lib/signed_xml.rb
+++ lib/signed_xml.rb
@@ -8,6 +8,23 @@ module SignedXml
     Document.new(thing)
   end
 
+  # Logger that does nothing
+  BITBUCKET_LOGGER = Logger.new(nil)
+  class << BITBUCKET_LOGGER
+    def add(*args)
+    end
+  end
+
+  # The logger signed_xml should use
+  def self.logger
+    @@logger ||= BITBUCKET_LOGGER
+  end
+
+  # Set the logger for signed_xml
+  def self.logger=(a_logger)
+    @@logger = a_logger
+  end
+
   autoload :Transformable, 'signed_xml/transformable'
   autoload :Document, 'signed_xml/document'
   autoload :Signature, 'signed_xml/signature'
--- lib/signed_xml/logging.rb
+++ lib/signed_xml/logging.rb
@@ -1,23 +1,6 @@
 module SignedXml
   # Logging stuff borrowed from https://github.com/pezra/saml-sp
 
-  # Logger that does nothing
-  BITBUCKET_LOGGER = Logger.new(nil)
-  class << BITBUCKET_LOGGER
-    def add(*args)
-    end
-  end
-
-  # The logger signed_xml should use
-  def self.logger
-    @@logger ||= BITBUCKET_LOGGER
-  end
-
-  # Set the logger for signed_xml
-  def self.logger=(a_logger)
-    @@logger = a_logger
-  end
-
   module Logging
     def logger
       SignedXml.logger
